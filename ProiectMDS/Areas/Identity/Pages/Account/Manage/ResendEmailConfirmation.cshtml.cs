cbc83fd59e3ea7c551d1e7e7f481874c00d847ca
AlexCretu20 <alexmari.cretu@gmail.com>
2025-05-05 18:19:51 +0300

    Bug email resolved

---
+++ ProiectMDS/Areas/Identity/Pages/Account/Manage/ResendEmailConfirmation.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProiectMDS.Areas.Identity.Pages.Account.Manage
+{
+    
+    public class ResendEmailConfirmationModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IEmailSender _emailSender;
+
+        public ResendEmailConfirmationModel(UserManager<IdentityUser> userManager, IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _emailSender = emailSender;
+        }
+       
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return RedirectToPage("/Products");
+            }
+
+            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            var callbackUrl = Url.Page(
+            "/Account/ConfirmEmail",
+            null,
+            new { userId = user.Id, code },
+            Request.Scheme);
+
+            await _emailSender.SendEmailAsync(
+                user.Email,
+                "Confirmă-ți emailul",
+                $"Te rugăm să confirmi contul apăsând <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>aici</a>.");
+
+            return Page();
+
+        }
+    }
+}
--- ProiectMDS/Services/EmailService.cs
+++ ProiectMDS/Services/EmailService.cs
@@ -11,7 +11,7 @@ namespace ProiectMDS.Services
         private readonly string _smtpHost = "smtp.gmail.com";
         private readonly int _smtpPort = 587;
         private readonly string _senderEmail = "proiect778@gmail.com";
-        private readonly string _senderPassword = "sgvrazcfwfyrlkak";
+        private readonly string _senderPassword = "oktpaturufmzgodn";
 
         public async Task SendEmailAsync(string toEmail, string subject, string body)
         {
@@ -19,16 +19,26 @@ namespace ProiectMDS.Services
             var myEmail = "proiect778@gmail.com";
             mail.From = new MailAddress(_senderEmail);
             mail.To.Add(myEmail);
+            mail.To.Add(toEmail);
             mail.Subject = subject;
             mail.Body = body;
             mail.IsBodyHtml = true;
 
             using (var smtp = new SmtpClient(_smtpHost, _smtpPort))
             {
+                smtp.UseDefaultCredentials = false;
                 smtp.Credentials = new NetworkCredential(_senderEmail, _senderPassword);
                 smtp.EnableSsl = true;
 
-                await smtp.SendMailAsync(mail);
+                //await smtp.SendMailAsync(mail);
+                try
+                {
+                    await smtp.SendMailAsync(mail);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Eroare la trimiterea emailului: " + ex.Message);
+                }
             }
         }
     }
